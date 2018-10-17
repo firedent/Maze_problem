83cc7f685c12ed257a1bb2bfec13972cf13d8837
zscself <zscself@gmail.com>
2018-10-17 20:41:05 +1100

    inaccessible point & area 成功

--- maze.py
+++ maze.py
@@ -1,6 +1,6 @@
 # Insert your code here
 from queue_adt import *
-
+from pathlib import Path
 
 class MazeError(Exception):
     pass
@@ -56,7 +56,8 @@ class Maze:
         >>> m.grid_maze_raw[0][7]
         0
         """
-        with open(filename) as f:
+        self.file_path = Path(filename)
+        with open(self.file_path) as f:
             lines = f.read().splitlines()
         # 找长度
         length = 0
@@ -175,7 +176,8 @@ class Maze:
             children.append(operators[d]((x, y)))
         return children
 
-    def __traversal_by_bfs(self, x, y, grid):
+    def __traversal_by_bfs(self, x, y, grid, x_l=0, x_r=0, y_t=0, y_b=0):
+        # x_l=0, x_r=0, y_t=0, y_b=0修剪grid，分变为：左、右、上、下。
         """
         >>> m = Maze('maze_1.txt')
         >>> m.grid_point_no_object = m._Maze__int_grid({0: '',1: 'E',2: 'S',3: 'SE'})
@@ -184,12 +186,23 @@ class Maze:
         [(2, 0), (2, 1), (2, 2), (3, 0), (3, 1), (3, 2), (4, 1), (4, 2), (4, 3), (5, 2), (6, 0), (6, 1), (6, 2), (7, 0)]
         >>> sorted(m._Maze__traversal_by_bfs(1,3,m.grid_point_no_object))
         [(1, 3)]
-        >>> sorted(m._Maze__traversal_by_bfs(2,0,m.grid_inner_point_no_object))
+        >>> sorted(m._Maze__traversal_by_bfs(2,0,m.grid_inner_point_no_object,x_r=1,y_b=1))
         [(2, 0), (2, 1), (3, 1)]
-        >>> sorted(m._Maze__traversal_by_bfs(3,0,m.grid_inner_point_no_object))
+        >>> sorted(m._Maze__traversal_by_bfs(3,0,m.grid_inner_point_no_object,x_r=1,y_b=1))
         [(3, 0), (4, 0), (4, 1), (5, 0), (5, 1)]
-        >>> sorted(m._Maze__traversal_by_bfs(0,1,m.grid_inner_point_no_object))
+        >>> sorted(m._Maze__traversal_by_bfs(0,1,m.grid_inner_point_no_object,x_r=1,y_b=1))
         [(0, 1)]
+        >>> m = Maze('maze_2.txt')
+        >>> m.grid_point_no_object = m._Maze__int_grid({0: '',1: 'E',2: 'S',3: 'SE'})
+        >>> m.grid_inner_point_no_object = m._Maze__int_grid({0: 'NW',1: 'W',2: 'N',3: ''})
+        >>> sorted(m._Maze__traversal_by_bfs(4,2,m.grid_point_no_object))
+        [(0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (1, 1), (1, 2), (1, 3), (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (4, 0), (4, 1), (4, 2), (4, 3), (4, 4)]
+        >>> sorted(m._Maze__traversal_by_bfs(4,0,m.grid_inner_point_no_object,x_r=1,y_b=1))
+        [(3, 0), (3, 1), (4, 0), (4, 1), (4, 2), (4, 3)]
+        >>> sorted(m._Maze__traversal_by_bfs(10,1,m.grid_inner_point_no_object,x_r=1,y_b=1))
+        [(9, 2), (10, 1), (10, 2)]
+        >>> sorted(m._Maze__traversal_by_bfs(10,3,m.grid_inner_point_no_object,x_r=1,y_b=1))
+        [(10, 3)]
         """
         visited_point = set()
         q = Queue()
@@ -198,7 +211,8 @@ class Maze:
             p = q.dequeue()
             visited_point.add(p)
             for c in self.__get_children(p[0], p[1], grid):
-                if 0 <= c[0] <= self.x_dim - 1 and 0 <= c[1] <= self.y_dim - 1:
+                if 0 + x_l <= c[0] <= self.x_dim - 1 - x_r \
+                        and 0 + y_t <= c[1] <= self.y_dim - 1 - y_b:
                     if (c[0], c[1]) not in visited_point:
                         q.enqueue(c)
 
@@ -214,18 +228,26 @@ class Maze:
         >>> maze.analyse()
         The maze has 12 gates.
         The maze has 8 sets of walls that are all connected.
+        The maze has 2 inaccessible inner points.
+        The maze has 4 accessible areas.
         >>> maze = Maze('maze_2.txt')
         >>> maze.analyse()
         The maze has 20 gates.
         The maze has 4 sets of walls that are all connected.
+        The maze has 4 inaccessible inner points.
+        The maze has 13 accessible areas.
         >>> maze = Maze('labyrinth.txt')
         >>> maze.analyse()
         The maze has 2 gates.
         The maze has 2 sets of walls that are all connected.
+        The maze has no inaccessible inner point.
+        The maze has a unique accessible area.
         >>> maze = Maze('bianjie.txt')
         >>> maze.analyse()
         The maze has 2 gates.
         The maze has walls that are all connected.
+        The maze has no inaccessible inner point.
+        The maze has a unique accessible area.
         """
 
         directions_of_point = {
@@ -244,24 +266,22 @@ class Maze:
         self.grid_inner_point_no_object = self.__int_grid(directions_of_inner_point)
 
         # ------------GATE------------
-        # 可以删掉
+        # gate 第三四问，求accessible area需要用
         gate_set = set()
-        # end 可以删掉
         num_gate = 0
         for i in [(0, 'W'), (self.x_dim - 2, 'E')]:
             for j in range(self.y_dim - 1):
                 if i[1] in self.grid_inner_point_no_object[i[0]][j]:
                     num_gate += 1
-                    # 可以删掉
                     gate_set.add(((i[0], j), i[1]))
-                    # end 可以删掉
         for i in range(self.x_dim - 1):
             for j in [(0, 'N'), (self.y_dim - 2, 'S')]:
                 if j[1] in self.grid_inner_point_no_object[i][j[0]]:
-                    num_gate += 1
-                    # 可以删掉
+                    # 等同于 num_gate = len(gate_set)
+                    # num_gate += 1
                     gate_set.add(((i, j[0]), j[1]))
-                    # end 可以删掉
+                    # ((x轴,y轴),朝向)
+        num_gate = len(gate_set)
         # 字典映射
         gate_output_dict = {
             0: 'no gate.',
@@ -282,13 +302,60 @@ class Maze:
                         num_set_of_wall += 1
                     visited_point_for_wall |= vp
         # 字典映射
-        gate_output_dict = {
+        wall_output_dict = {
             0: 'no wall.',
             1: 'walls that are all connected.'
         }
         print(f'The maze has '
-              f'{gate_output_dict.get(num_set_of_wall,f"{num_set_of_wall} sets of walls that are all connected.")}')
+              f'{wall_output_dict.get(num_set_of_wall,f"{num_set_of_wall} sets of walls that are all connected.")}')
         # ------------END WALL------------
 
+        # ------------ACCESSIBLE AREA------------
+        # ------------INACCESSIBLE AREA------------
+        num_accessible_area = 0
+        num_inaccessible_inner_point = 0
+        visited_inner_point_for_path = set()
+        for g in gate_set:
+            if g[0] not in visited_inner_point_for_path:
+                vip = self.__traversal_by_bfs(g[0][0], g[0][1], self.grid_inner_point_no_object, x_r=1, y_b=1)
+                num_accessible_area += 1
+                visited_inner_point_for_path |= vip
+        num_inaccessible_inner_point = (self.y_dim-1)*(self.x_dim-1) - len(visited_inner_point_for_path)
+        accessible_area_output_dict = {
+            0: 'no accessible area.',
+            1: 'a unique accessible area.'
+        }
+        inaccessible_point_output_dict = {
+            0: 'no inaccessible inner point.',
+            1: 'a unique inaccessible inner point.'
+        }
+        t_str = f"{num_inaccessible_inner_point} inaccessible inner points."
+        print(f'The maze has '
+              f'{inaccessible_point_output_dict.get(num_inaccessible_inner_point, t_str)}')
+        print(f'The maze has '
+              f'{accessible_area_output_dict.get(num_accessible_area, f"{num_accessible_area} accessible areas.")}')
+        # ------------END INACCESSIBLE AREA------------
+        # ------------END ACCESSIBLE AREA------------
+
     def display(self):
-        pass
+        pillar_set = set()
+        # 遍历grid_point，找到pillar
+        for i in range(self.x_dim):
+            for j in range(self.y_dim):
+                if self.grid_point_no_object[i][j] is '':
+                    pillar_set.add((i,j))
+        tex_content_head = '\\documentclass[10pt]{article}\n' \
+                           '\\usepackage{tikz}\n' \
+                           '\\usetikzlibrary{shapes.misc}\n' \
+                           '\\usepackage[margin=0cm]{geometry}\n' \
+                           '\\pagestyle{empty}\n' \
+                           '\\tikzstyle{every node}=[cross out, draw, red]\n' \
+                           '\\begin{document}\n' \
+                           '\\vspace*{\\fill}\n' \
+                           '\n' \
+                           '\\begin{center}\n' \
+                           '\n' \
+                           '\\begin{tikzpicture}[x=0.5cm, y=-0.5cm, ultra thick, blue]'
+        # 写入result文件
+        # with open(self.file_path.stem + '.tex') as f:
+        with open(self.file_path.stem + '.tex') as f:
